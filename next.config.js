b99e84701464f961df71d6f3ce9e38848c1c7a8b
root <root@localhost.localdomain>
2026-06-11 02:03:18 +0700

    force redeploy: add no-cache headers for bujuk

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/bujuk/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
