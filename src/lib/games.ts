d51f5d2815f30888f47dff22e7c5227e1fb9bc1c
rinonism <rinonism@users.noreply.github.com>
2026-07-20 01:00:21 +0700

    chore: auto-sync digiflazz prices

--- src/lib/games.ts
+++ src/lib/games.ts
@@ -259,7 +259,7 @@ export const games: Game[] = [
     denominations: [
       { amount: "475 VP", price: 54100, label: "475 VP", sku: "v475" },
       { amount: "950 VP", price: 108200, label: "950 VP", sku: "v950" },
-      { amount: "1.475 VP", price: 162400, label: "1.475 VP", sku: "v1475" },
+      { amount: "1.475 VP", price: 162500, label: "1.475 VP", sku: "v1475" },
       { amount: "2.050 VP", price: 215200, label: "2.050 VP", sku: "v2050" },
       { amount: "3.050 VP", price: 323100, label: "3.050 VP", sku: "v3050" },
       { amount: "3.650 VP", price: 374100, label: "3.650 VP", sku: "v3650" },
