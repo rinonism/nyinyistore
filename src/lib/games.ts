f5ac8b40a877ff99c6413e1009588f17dff13502
rinonism <rinonism@users.noreply.github.com>
2026-08-28 13:01:13 +0700

    chore: auto-sync digiflazz prices

--- src/lib/games.ts
+++ src/lib/games.ts
@@ -84,8 +84,8 @@ export const games: Game[] = [
       { amount: "1446 Diamonds", price: 418200, label: "1446 💎", sku: "ml1446" },
       { amount: "1516 Diamonds", price: 430000, label: "1516 💎", sku: "ml1516" },
       { amount: "1584 Diamonds", price: 449500, label: "1584 💎", sku: "ml1584" },
-      { amount: "1647 Diamonds", price: 472400, label: "1647 💎", sku: "ml1647" },
-      { amount: "1672 Diamonds", price: 478900, label: "1672 💎", sku: "ml1672" },
+      { amount: "1647 Diamonds", price: 472300, label: "1647 💎", sku: "ml1647" },
+      { amount: "1672 Diamonds", price: 478800, label: "1672 💎", sku: "ml1672" },
     ],
   },
   {
