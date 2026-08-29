7344bfde2d7518504a91021a2685cf6d20b2e766
rinonism <rinonism@users.noreply.github.com>
2026-08-29 12:00:06 +0700

    chore: auto-sync digiflazz prices

--- src/lib/games.ts
+++ src/lib/games.ts
@@ -50,7 +50,7 @@ export const games: Game[] = [
       { amount: "Monthly Epic Bundle", price: 85500, label: "Monthly Epic Bundle", sku: "mlbndle" },
       { amount: "275 Diamonds", price: 80700, label: "275 💎", sku: "ml275" },
       { amount: "Coupon Pass", price: 73900, label: "Coupon Pass", sku: "mlcoupon" },
-      { amount: "284 Diamonds", price: 81800, label: "284 💎", sku: "ml284" },
+      { amount: "284 Diamonds", price: 82300, label: "284 💎", sku: "ml284" },
       { amount: "305 Diamonds", price: 89400, label: "305 💎", sku: "ml305" },
       { amount: "Starlight Member", price: 89800, label: "Starlight Member", sku: "mlstar" },
       { amount: "344 Diamonds", price: 102900, label: "344 💎", sku: "ml344" },
