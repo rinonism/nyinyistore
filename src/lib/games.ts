ec9067fa6f5c7d5b49d6a2085475357ff45dc796
rinonism <rinonism@users.noreply.github.com>
2026-08-30 12:01:15 +0700

    chore: auto-sync digiflazz prices

--- src/lib/games.ts
+++ src/lib/games.ts
@@ -38,7 +38,7 @@ export const games: Game[] = [
       { amount: "Weekly Diamond Pass", price: 32400, label: "Weekly Diamond Pass", sku: "mlweek" },
       { amount: "Weekly Diamond Pass 2x", price: 64100, label: "Weekly Diamond Pass 2x", sku: "mlweek2" },
       { amount: "Weekly Diamond Pass 3x", price: 97100, label: "Weekly Diamond Pass 3x", sku: "mlweek3" },
-      { amount: "Weekly Diamond Pass 4x", price: 128200, label: "Weekly Diamond Pass 4x", sku: "mlweek4" },
+      { amount: "Weekly Diamond Pass 4x", price: 129500, label: "Weekly Diamond Pass 4x", sku: "mlweek4" },
       { amount: "Weekly Diamond Pass 5x", price: 161800, label: "Weekly Diamond Pass 5x", sku: "mlweek5" },
       { amount: "86 Diamonds", price: 22000, label: "86 💎", sku: "ML86." },
       { amount: "112 Diamonds", price: 33300, label: "112 💎", sku: "ml112" },
